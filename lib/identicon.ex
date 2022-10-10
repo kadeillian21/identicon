aa502c15f0fda7f4d28b3491c499ee0437f69345
Kade Illian <kadeillianmt@gmail.com>
2022-10-10 12:40:49 -0600

    added a main and hash_input method that takes in a string and returns that string as a hash of 15 different numbers

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,7 +4,7 @@ defmodule Identicon do
   """
 
   @doc """
-  Hello world.
+  Identicon generates a custom 'identicon' for the user based on an input string.  The string is hashed and converted into a series of integers, which generates a unique 'identicon'.  Identicons are 250px by 250px, have a 5x5 grid, and the image mirrors itself across the x-axis.  Identicons colors are also randomly generated.
 
   ## Examples
 
@@ -12,7 +12,13 @@ defmodule Identicon do
       :world
 
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input()
+  end
+
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list()
   end
 end
